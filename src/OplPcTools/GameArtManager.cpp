f75941278b1d8bcc2e0645b7346fd620306821ca
brainstream <brainstream@yandex.ru>
2019-12-08 13:25:36 +0300

    Copyright updated

--- src/OplPcTools/GameArtManager.cpp
+++ src/OplPcTools/GameArtManager.cpp
@@ -1,5 +1,5 @@
 /***********************************************************************************************
- * Copyright © 2017-2018 Sergey Smolyannikov aka brainstream                                   *
+ * Copyright © 2017-2019 Sergey Smolyannikov aka brainstream                                   *
  *                                                                                             *
  * This file is part of the OPL PC Tools project, the graphical PC tools for Open PS2 Loader.  *
  *                                                                                             *
